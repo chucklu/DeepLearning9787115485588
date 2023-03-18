2474609edb9c2eddc43eb8c2bf4a084851d5a63c
Chuck LU <chucklu2015@outlook.com>
2023-03-19 02:46:50 +0800

    set dtype explicitly

--- chapter1/numpy_test_2023_0319_001.py
+++ chapter1/numpy_test_2023_0319_001.py
@@ -3,3 +3,8 @@ A = np.array([[1, 2], [3, 4]])
 print(f'A = {A}')
 print(f'A.shape = {A.shape}')
 print(f'A.dtype = {A.dtype}')
+
+A1 = np.array([[1, 2], [3, 4]], dtype=np.int64)
+print(f'A1 = {A1}')
+print(f'A1.shape = {A1.shape}')
+print(f'A1.dtype = {A1.dtype}')
