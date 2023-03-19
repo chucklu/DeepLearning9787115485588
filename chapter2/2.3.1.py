7be07ae204cc4f7cc750ccbcad2d1b339cf16171
Chuck LU <chucklu2015@outlook.com>
2023-03-19 13:31:03 +0800

    add code which simulate AND gate with Perceptron

---
+++ chapter2/2.3.1.py
@@ -0,0 +1,16 @@
+def AND(x1, x2):
+    w1, w2, theta = 0.5, 0.5, 0.7
+    temp = w1*x1+w2*x2
+    if (temp < theta):
+        return 0
+    else:
+        return 1
+
+
+def test():
+    for i in range(0, 2):
+        for j in range(0, 2):
+            print(f'AND({i},{j})={AND(i,j)}')
+
+
+test()
