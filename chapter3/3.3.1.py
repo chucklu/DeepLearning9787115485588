7fd5aa9e9360816cb97a7c163094a3079f549c79
Chuck LU <chucklu2015@outlook.com>
2023-03-21 23:40:05 +0800

    two dimension array

--- chapter3/3.3.1.py
+++ chapter3/3.3.1.py
@@ -4,3 +4,8 @@ print(f'A = {A}')
 print(f'A.ndim = {A.ndim}')
 print(f'A.shape = {A.shape}')
 print(f'A.shape[0] = {A.shape[0]}')
+
+B=np.array([[1,2],[3,4],[5,6]])
+print(f'B = {B}')
+print(f'B.ndim = {B.ndim}')
+print(f'B.shape = {B.shape}')
